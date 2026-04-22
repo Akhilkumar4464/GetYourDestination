2d20f592d73cb3958a9f5ea730e6fcf4f079c3af
akhil kumar <niteshsharma4464@gmail.com>
2026-04-22 21:30:06 +0530

    feat: enhance authentication forms with improved styling and user experience

--- frontend/src/features/Auth/pages/Login.jsx
+++ frontend/src/features/Auth/pages/Login.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import { Link , useNavigate} from 'react-router-dom';
 import '../auth.form.scss';
 
   export default function  Login (){
+      const navigate = useNavigate();
     return (
     <main>
       <div className=" form-container">
-        <h1>Welcome to login page</h1>
+        <h1>welcome Back !</h1>
 <form>
+  
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter your email'/>
+    
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Enter your password'/>
   </div>
-  <button type="submit" className="btn btn-primary">login dev .</button>
+  <button type="submit" className="btn btn-primary">login </button>
 </form>
+       <p>Don't have an account ? <Link to="/register">Register</Link></p>
       </div>
     </main>
   
